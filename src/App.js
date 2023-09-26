f26c4eaad1a6a469c26a3d53b345f821b3c086d7
Sarah Sajid <sarahsajid19@gmail.com>
2023-09-26 23:14:19 +0500

    objects in jsx

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar from "./components/Navbar";
+import React from "react"; // Make sure to import React
+
+// objects in jsx
+let obj = {
+  userNAme: "sarah",
+  userAge: 10,
+  userCninc: "0000000000",
+  userEmail: "abc@gmail.com",
+  imageUrl:
+    "https://media.istockphoto.com/id/1146517111/photo/taj-mahal-mausoleum-in-agra.jpg?s=612x612&w=0&k=20&c=vcIjhwUrNyjoKbGbAQ5sOcEzDUgOfCsm9ySmJ8gNeRk=",
+  userAlt: "user image",
+};
+
+// creating a component in this file
+function Component1(props) {
+  return <h1>i am the h1 of first component</h1>;
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar />
+      {/* object in JSX */}
+      <h3>hey {obj.userNAme} </h3>
+      <h3>your age is : {obj.userAge} </h3>
+      <h3>your cninc is : {obj.userCninc} </h3>
+      <h3>your email is: {obj.userEmail} </h3>
+      <img src={obj.imageUrl} alt={obj.userAlt} width={100} height={100} />
+      {/* rendering Component1 */}
+      <Component1 />
+    </>
   );
 }
 
