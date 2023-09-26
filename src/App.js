f157dd2c43cd51c7668f2370d8f9e235b6270346
Sarah Sajid <sarahsajid19@gmail.com>
2023-09-26 23:44:03 +0500

    counter app using react

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react"; // Make sure to import React
 import Navbar from "./components/Navbar";
 import ObjectsInJsx from "./components/ObjectsInJsx";
+import CounterApp from "./components/CounterApp";
 
 function App() {
   return (
     <>
-      <Navbar />
-      <ObjectsInJsx />
+      {/* <Navbar title="counter app" /> */}
+      {/* <ObjectsInJsx /> */}
+      <CounterApp />
     </>
   );
 }
